9e61c09956413c25b9d20df11b10e0ad8040f35b
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-28 14:44:59 +0100

    feat: add RegisterComponent with template and test suite

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register.component',
+  imports: [],
+  templateUrl: './register.component.html',
+})
+export class RegisterComponent {}
