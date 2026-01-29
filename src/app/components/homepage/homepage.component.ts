168d9b5b2ba4cb45179f5c9a705cc34e5ed701a3
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-29 13:24:23 +0100

    feat: add homepage component with template and spec file

---
+++ src/app/components/homepage/homepage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage.component',
+  imports: [],
+  templateUrl: './homepage.component.html',
+  standalone: true,
+})
+export class HomepageComponent {}
