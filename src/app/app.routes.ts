8c258cfb31acab6bbf9a80cd5ce969cd9e9986c2
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-29 10:27:27 +0100

    feat: add profile route to application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { CartComponent } from './components/cart/cart.component';
 import { TrainingsComponent } from './components/trainings/trainings.component';
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 // Define the application routes
 export const routes: Routes = [
@@ -13,4 +14,7 @@ export const routes: Routes = [
 
   // Route for the registration page
   { path: 'register', component: RegisterComponent },
+
+  // Route for the profile page
+  { path: 'profile', component: ProfileComponent },
 ];
