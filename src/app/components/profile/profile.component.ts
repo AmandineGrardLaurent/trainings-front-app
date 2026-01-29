5c336eaad35c53548562fbe888985584a8ddfa23
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-29 10:35:35 +0100

    feat: enhance comments in profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserModel } from '../../models/user/user.model';
 import { UserService } from '../../services/user/user.service';
 
@@ -9,11 +9,17 @@ import { UserService } from '../../services/user/user.service';
   standalone: true,
 })
 export class ProfileComponent {
+  // Signal that holds the user state (read-only)
   userSignal;
+
+  // Local variable to hold the current value of the user
   userValue: UserModel | null = null;
 
   constructor(private userService: UserService) {
+    // Get the user signal from the UserService
     this.userSignal = this.userService.getUser();
+
+    // Read the current value of the signal and store it in a local variable
     this.userValue = this.userSignal();
   }
 }
