5e2203ef417ba9ee559fc07b58a0a847b34d24fb
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-29 08:58:49 +0100

    feat: implement registration form and user addition logic in RegisterComponent

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,8 +1,33 @@
+import { UserService } from './../../services/user/user.service';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register.component',
-  imports: [],
+  imports: [ReactiveFormsModule],
+  standalone: true,
   templateUrl: './register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+  ) {
+    this.registerForm = this.fb.group({
+      name: [''],
+      email: [''],
+      password: [''],
+    });
+  }
+
+  addUser() {
+    if (this.registerForm.valid) {
+      this.userService.register(this.registerForm.value);
+
+      // Reset the form after submit
+      this.registerForm.reset();
+    }
+  }
+}
