1aee51c1a889ff08c07712d5c3f4b9aaae97428f
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-03 09:23:38 +0100

    feat: create api-trainingService
    - add getTrainings method
    - add getTrainingById method

---
+++ src/app/services/api/api-training.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TrainingModel } from '../../models/training/training.model';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiTrainingService {
+  constructor(private http: HttpClient) {}
+
+  public getTrainings() {
+    return this.http.get<TrainingModel[]>(`${environment.apiUrl}/trainings`);
+  }
+
+  public getTrainingById(id: number) {
+    return this.http.get<TrainingModel>(`${environment.apiUrl}/trainings/${id}`);
+  }
+}
