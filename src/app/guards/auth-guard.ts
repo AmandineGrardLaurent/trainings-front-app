23218246c187e6fc8153f801351534d3f7193d73
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-03 13:32:08 +0100

    feat: add auth-guard

---
+++ src/app/guards/auth-guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../services/user/user.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const userService = inject(UserService);
+  const router = inject(Router);
+
+  console.log(userService.isLoggedIn());
+  if (!userService.isLoggedIn()) {
+    return router.createUrlTree(['/auth']);
+  }
+  return true;
+};
