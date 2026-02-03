02beae8a63660bd57f59bc8db6b7853c5d5840f6
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-03 13:46:25 +0100

    feat: modify profileComponent and implement logout method

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
 import { UserModel } from '../../models/user/user.model';
 import { UserService } from '../../services/user/user.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile.component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   standalone: true,
 })
 export class ProfileComponent {
-  // Signal that holds the user state (read-only)
-  userSignal;
-
   // Local variable to hold the current value of the user
   userValue: UserModel | null = null;
 
-  constructor(private userService: UserService) {
-    // Get the user signal from the UserService
-    this.userSignal = this.userService.getUser();
+  constructor(
+    private userService: UserService,
+    private router: Router,
+  ) {
+    this.userValue = this.userService.getUser();
+  }
 
-    // Read the current value of the signal and store it in a local variable
-    this.userValue = this.userSignal();
+  logout() {
+    this.userService.logoutUser();
+    alert('Déconnexion réussie !');
+    this.router.navigateByUrl('/');
   }
 }
