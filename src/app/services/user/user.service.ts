4afd3e4bc5bb83d8a0d8a725e8c500d6b1b17c6f
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-03 09:21:41 +0100

    feat: add verifyUserExists method in userService

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { UserModel } from '../../models/user/user.model';
 import { signal } from '@angular/core';
+import { ApiUserService } from '../api/api-user.service';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,11 +10,17 @@ import { signal } from '@angular/core';
 export class UserService {
   private user = signal<UserModel | null>(null);
 
+  constructor(private apiUserService: ApiUserService) {}
+
   register(user: UserModel) {
     this.user.set(user);
   }
 
   getUser() {
     return this.user.asReadonly();
+  }
+
+  verifyUserExists(email: string) {
+    return this.apiUserService.getUserByEmail(email).pipe(map((users) => users.length > 0));
   }
 }
