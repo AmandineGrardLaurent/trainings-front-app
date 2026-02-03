0ff028f7c6403fa94bcbea5c03ee440aec9f5ac8
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-03 09:22:32 +0100

    feat: create api-userService - add getUsers method - add getUserByEmail method

---
+++ src/app/services/api/api-user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { UserModel } from '../../models/user/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiUserService {
+  constructor(private http: HttpClient) {}
+
+  public getUsers() {
+    return this.http.get<UserModel[]>(`${environment.apiUrl}/users`);
+  }
+
+  public getUserByEmail(email: string) {
+    return this.http.get<UserModel[]>(`${environment.apiUrl}/users?email=${email.toLowerCase()}`);
+  }
+}
