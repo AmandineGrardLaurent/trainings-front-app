c88b6fc1801aae9ff97dae2680359b1da163464f
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-04 08:20:54 +0100

    feat: add methods in userService - getUser - setUser - loginUser - logoutUser - isLoggedIn

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { UserModel } from '../../models/user/user.model';
-import { signal } from '@angular/core';
 import { ApiUserService } from '../api/api-user.service';
 import { map } from 'rxjs';
 
@@ -8,17 +7,44 @@ import { map } from 'rxjs';
   providedIn: 'root',
 })
 export class UserService {
-  private user = signal<UserModel | null>(null);
+  private user: UserModel | null = null;
 
-  constructor(private apiUserService: ApiUserService) {}
+  constructor(private apiUserService: ApiUserService) {
+    // const storedUser = localStorage.getItem('userLoggedIn');
+    // if (storedUser) {
+    //   this.user = JSON.parse(storedUser);
+    // }
+  }
 
   register(user: UserModel) {
-    this.user.set(user);
+    console.log('Registering user:', user);
+    this.user = user;
   }
 
   getUser() {
-    return this.user.asReadonly();
+    return this.user;
   }
+
+  setUser(user: UserModel) {
+    this.user = user;
+    // localStorage.setItem('userLoggedIn', JSON.stringify(user));
+  }
+
+  loginUser(user: UserModel) {
+    this.user = user;
+    // localStorage.setItem('userLoggedIn', JSON.stringify(user));
+  }
+
+  logoutUser() {
+    this.user = null;
+    // localStorage.removeItem('userLoggedIn');
+  }
+
+  isLoggedIn() {
+    return this.user !== null;
+  }
+
+  isAdmin() {}
 
   verifyUserExists(email: string) {
     return this.apiUserService.getUserByEmail(email).pipe(map((users) => users.length > 0));
