4c251116c3b5f0f67037d780f419578fc360d8b3
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-04 14:46:37 +0100

    docs: add documentation comments to userList component

--- src/app/components/admin/user-list/user-list.component.ts
+++ src/app/components/admin/user-list/user-list.component.ts
@@ -2,6 +2,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UserModel } from '../../../models/user/user.model';
 import { CommonModule } from '@angular/common';
 
+/**
+ * Component responsible for displaying a list of users.
+ *
+ * This component receives a list of users from a parent component
+ * and emits events when a user needs to be deleted.
+ */
 @Component({
   selector: 'app-user-list',
   imports: [CommonModule],
@@ -9,6 +15,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './user-list.component.html',
 })
 export class UserListComponent {
+  /**
+   * List of users to display.
+   * Provided by the parent component via property binding.
+   */
   @Input() users: UserModel[] = [];
+
+  /**
+   * Event emitted when a user is requested to be deleted.
+   * Emits the ID of the user.
+   */
   @Output() deleteUser = new EventEmitter<number>();
 }
