296355d73f32c6397243c4b5d6be676f1857fb84
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-04 14:08:29 +0100

    feat: add @output in trainingListComponent

--- src/app/components/admin/training-list/training-list.component.ts
+++ src/app/components/admin/training-list/training-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TrainingModel } from '../../../models/training/training.model';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-training-list',
   imports: [CommonModule],
+  standalone: true,
   templateUrl: './training-list.component.html',
 })
 export class TrainingListComponent {
   @Input() trainings: TrainingModel[] = [];
+  @Output() deleteTraining = new EventEmitter<number>();
 }
