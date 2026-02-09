10596863b15540d24454c1fd1b08fb494f2934ff
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-09 11:29:41 +0100

    feat: add getUserId method in userService

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { UserModel } from '../../models/user/user.model';
 import { ApiUserService } from '../api/api-user.service';
 import { map } from 'rxjs';
-import { CartService } from '../cart/cart.service';
 
 /**
  * UserService
@@ -38,6 +37,10 @@ export class UserService {
   // Returns the currently logged-in user
   getUser() {
     return this.user;
+  }
+
+  getUserId() {
+    return this.user?.id;
   }
 
   // Sets the current user in the service state
