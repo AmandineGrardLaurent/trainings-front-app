c2644e57d5a833fcf62229cb221f49225d5850b0
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-09 11:32:20 +0100

    feat: add getOrders method and ngOnInit in profileComponent.ts

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,29 +1,50 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { UserModel } from '../../models/user/user.model';
 import { UserService } from '../../services/user/user.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { ApiOrderService } from '../../services/api/api-order.service';
+import { OrderModel } from '../../models/order/order.model';
+import { OrdersComponent } from '../orders/orders.component';
 
 @Component({
   selector: 'app-profile.component',
-  imports: [CommonModule],
+  imports: [CommonModule, OrdersComponent],
   templateUrl: './profile.component.html',
   standalone: true,
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   // Local variable to hold the current value of the user
   userValue: UserModel | null = null;
+  listOrders: OrderModel[] = [];
 
   constructor(
     private userService: UserService,
     private router: Router,
+    private apiOrderService: ApiOrderService,
+    private cdr: ChangeDetectorRef,
   ) {
     this.userValue = this.userService.getUser();
+  }
+
+  ngOnInit(): void {
+    const userId = this.userService.getUserId();
+    this.getOrders(Number(userId));
   }
 
   logout() {
     this.userService.logoutUser();
     alert('Déconnexion réussie !');
     this.router.navigateByUrl('/');
+  }
+
+  getOrders(userId: number) {
+    this.apiOrderService.getOrdersByUser(userId).subscribe({
+      next: (listOrders) => {
+        this.listOrders = listOrders;
+        this.cdr.detectChanges();
+      },
+      error: (err) => console.error('Error fetching orders:', err.message),
+    });
   }
 }
